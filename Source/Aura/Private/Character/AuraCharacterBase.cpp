fe1641f0104930ecef662af525a2ade5e4d3dff6
LeeTaes <apth1023@naver.com>
2025-01-31 12:38:45 +0900

    Character Blueprint Setup

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -7,6 +7,11 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = true;
 
+	// 무기를 장착하기 위한 컴포넌트를 생성하고 기본 메쉬에 부착시켜줍니다.
+	// - 무기는 WeaponHandSocket에 부착되어야 하며, 충돌하지 않도록 설정합니다.
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("Weapon"));
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 void AAuraCharacterBase::BeginPlay()
--- Source/Aura/Public/Character/AuraCharacterBase.h
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -17,4 +17,12 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+protected:
+	// TObjectPtr와 원시 포인터의 차이
+	// - 기본적으로 일반적인 포인터처럼 동작하지만 에디터에서 몇 가지 추가 기능을 가짐
+	// 1. 접근 추적(access tracking): 특정 포인터가 얼마나 자주 접근되거나 역참조되는지를 추적할 수 있습니다.
+	// 2. 지연 로드(lazy loading): 실제로 필요할 때까지 에셋을 로드하지 않고 대기할 수 있도록 합니다.
+	// 현재 언리얼 엔진에서는 멤버 변수로 TObjectPtr을 사용하는 방향으로 표준이 이동하고 있습니다.
+	UPROPERTY(EditAnywhere, Category = "Combat")
+	TObjectPtr<class USkeletalMeshComponent> Weapon;
 };
