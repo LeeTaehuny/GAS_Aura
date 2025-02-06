9d0008765502e5eb5d81c4ff3aaae2c2adc423e7
LeeTaes <apth1023@naver.com>
2025-02-07 08:52:31 +0900

    Highlight 로직 구현

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Player/AuraPlayerController.h"
+#include "Interaction/EnemyInterface.h"
 
 #include "EnhancedInputSubsystems.h"
 #include "EnhancedInputComponent.h"
@@ -11,6 +12,66 @@ AAuraPlayerController::AAuraPlayerController()
 {
 	// 복제되도록 설정합니다.
 	bReplicates = true;
+}
+
+void AAuraPlayerController::PlayerTick(float DeltaTime)
+{
+	Super::PlayerTick(DeltaTime);
+
+	// 커서 아래의 적을 체크하는 것은 비용이 많이 소모되지 않음
+	// * 매 프레임 적을 체크합니다.
+	CursorTrace();
+}
+
+void AAuraPlayerController::CursorTrace()
+{
+	FHitResult CursorHit;
+	// 커서 아래의 물체를 체크합니다.
+	GetHitResultUnderCursor(ECollisionChannel::ECC_Visibility, false, CursorHit);
+
+	// 물체가 없는 경우 종료합니다.
+	if (!CursorHit.bBlockingHit) return;
+
+	// 현재 저장된 액터를 이전 액터로 변경하고 현재 액터는 새로 받아옵니다.
+	LastActor = ThisActor;
+	// * 원시 포인터의 경우 이렇게 매 프레임 Cast를 통해 체크를 해야 함
+	//   ThisActor = Cast<IEnemyInterface>(CursorHit.GetActor());
+	// * TScriptInterface를 사용하면 Cast를 사용하지 않아도 됨(효율적)
+	ThisActor = CursorHit.GetActor();
+
+	// A: 이전 액터가 null이고, 현재 액터도 null인 경우						- 아무것도 하지 않음
+	// B: 이전 액터가 null이고, 현재 액터는 존재하는 경우					- 현재 액터만 HighlightActor() 함수 실행
+	// C: 이전 액터는 존재하고, 현재 액터는 null인 경우						- 이전 액터만 UnHighlightActor() 함수 실행
+	// D: 이전 액터도 존재하고, 현재 액터도 존재하는 경우 (두 액터가 다름)	- 이전 액터 UnHighlightActor(), 현재 액터 HighlightActor() 함수 실행
+	// E: 이전 액터도 존재하고, 현재 액터도 존재하는 경우 (두 액터가 같음)  - 아무것도 하지 않음
+
+	if (LastActor == nullptr)
+	{
+		if (ThisActor != nullptr)
+		{
+			// Case B
+			ThisActor->HighlightActor();
+		}
+		else {}// Case A : Do Noting
+	}
+	else
+	{
+		if (ThisActor == nullptr)
+		{
+			// Case C
+			LastActor->UnHighlightActor();
+		}
+		else
+		{
+			if (LastActor != ThisActor)
+			{
+				// Case D
+				LastActor->UnHighlightActor();
+				ThisActor->HighlightActor();
+			}
+			else {} // Case E : Do Noting
+		}
+	}
 }
 
 void AAuraPlayerController::BeginPlay()
--- Source/Aura/Public/Player/AuraPlayerController.h
+++ Source/Aura/Public/Player/AuraPlayerController.h
@@ -17,6 +17,9 @@ class AURA_API AAuraPlayerController : public APlayerController
 public:
 	AAuraPlayerController();
 
+	// 플레이어 컨트롤러에서의 Ticjk 함수
+	virtual void PlayerTick(float DeltaTime) override;
+
 protected:
 	virtual void BeginPlay() override;
 	virtual void SetupInputComponent() override;
@@ -30,4 +33,12 @@ private:
 
 	// struct를 붙여 구조체 또한 전방 선언이 가능합니다.
 	void Move(const struct FInputActionValue& InputActionValue);
+
+private:
+	// 커서 아래의 적 유무를 판별하기 위한 함수
+	void CursorTrace();
+
+	// 원시 포인터 대신 TScriptInterface를 사용해 안전하게 관리합니다.
+	TScriptInterface<class IEnemyInterface> LastActor;
+	TScriptInterface<class IEnemyInterface> ThisActor;
 };
