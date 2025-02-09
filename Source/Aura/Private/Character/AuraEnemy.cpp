9c97bce8fcc0322d481b41f7ece370d8778df782
LeeTaes <apth1023@naver.com>
2025-02-10 08:45:21 +0900

    Highlight 구현 완료

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,13 +2,28 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	// 메쉬의 충돌 채널을 설정합니다. (Visibility 충돌 허용)
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	bHighlighted = true;
+	// 캐릭터 메쉬의 Custom Depth를 활성화하고 250의 값을 설정합니다.
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	// 무기 메쉬의 Custom Depth를 활성화하고 250의 값을 설정합니다.
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bHighlighted = false;
+	// 캐릭터 메쉬의 Custom Depth를 비활성화합니다.
+	GetMesh()->SetRenderCustomDepth(false);
+	// 무기 메쉬의 Custom Depth를 비활성화합니다.
+	Weapon->SetRenderCustomDepth(false);
 }
