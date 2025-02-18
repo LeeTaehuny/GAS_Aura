034cd9fd6c624efea2bdc288eb92e42faebe2e91
LeeTaes <apth1023@naver.com>
2025-02-18 12:26:07 +0900

    Attribute Setting
    체력, 최대체력, 마나, 최대마나 속성 생성 및 복제 설정

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,49 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "AbilitySystemComponent.h"
+#include "Net/UnrealNetwork.h"
 
+UAuraAttributeSet::UAuraAttributeSet()
+{
+}
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	// 특정 조건에 의해 복제되는 변수 등록
+	// * 3 : Condition_None : 항상 복제
+	// * 4 : REPNOTIFY_Always : 값이 변경되지 않아도 복제할 때마다 OnRep_Health() 함수 실행
+	// * 4 : REPNOTIFY_OnChanged : 값이 변경될 때만 OnRep_Health() 함수 실행
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, Mana, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet, MaxMana, COND_None, REPNOTIFY_Always);
+}
+
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth) const
+{
+	// AttributeSystem에 해당 변경을 등록합니다.
+	// * 변경 추적 가능
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Health , OldHealth);
+}
+
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const
+{
+	// AttributeSystem에 해당 변경을 등록합니다.
+	// * 변경 추적 가능
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void UAuraAttributeSet::OnRep_Mana(const FGameplayAttributeData& OldMana) const
+{
+	// AttributeSystem에 해당 변경을 등록합니다.
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, Mana, OldMana);
+}
+
+void UAuraAttributeSet::OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const
+{
+	// AttributeSystem에 해당 변경을 등록합니다.
+	GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet, MaxMana, OldMaxMana);
+}
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -14,4 +14,42 @@ class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+	UAuraAttributeSet();
+
+protected:
+	// Replicate를 사용하기 위해 변수를 등록하는 함수
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+
+public:
+	// GAS에서 사용하는 Attribute는 FGameplayAttributeData로 관리됩니다.
+	// 해당 변수가 서버에서 변경되면 복제가 일어나도록 매크로를 추가합니다.
+	// * 체력
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_Health)
+	FGameplayAttributeData Health;
+	// * 최대 체력
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_MaxHealth)
+	FGameplayAttributeData MaxHealth;
+	// * 마나
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_Mana)
+	FGameplayAttributeData Mana;
+	// * 최대 마나
+	UPROPERTY(BlueprintReadOnly, Category = "Vital Attributes", ReplicatedUsing = OnRep_MaxMana)
+	FGameplayAttributeData MaxMana;
+
+
+public:
+	// 체력이 복제되면 실행되는 함수(클라이언트)
+	// * OldHealth : 기존 클라이언트에서 가지고 있던 Health값(서버 X)
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	// 최대 체력이 복제되면 실행되는 함수(클라이언트)
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	// 마나가 복제되면 실행되는 함수(클라이언트)
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	// 최대 마나가 복제되면 실행되는 함수(클라이언트)
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 };
